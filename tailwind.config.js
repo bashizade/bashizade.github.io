6c77ba4aae3765d830afd463564ce3166410a80b
bashizade <bashizade.official@gmail.com>
2024-02-18 11:54:00 +0330

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui:{
-    themes: ["dark","light","dracula","retro","coffee","night","forest"],
+    themes: ["luxury","retro"],
   }
 }
 
